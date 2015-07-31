b002434bf1bb4a9f5954d5cfeb6aa951fe82a024
Houshalter <Houshalter@gmail.com>
2015-07-31 12:24:13 -0400

    added features

--- lw-replay.lua
+++ lw-replay.lua
@@ -3,6 +3,7 @@ package.path = package.path..";./?.lua;./?/init.lua;"
 require 'config'
 require 'irc'
 cjson = require 'cjson'
+require 'quotes'
 
 
 
@@ -13,11 +14,35 @@ local s = irc.new{nick = config.nick, username = config.username, realname = con
 
 s:hook('OnChat', function(user, channel, message)
 	if message:match('%.ff') then
-		newspeed = tonumber(message:match('%.ff (.*)'))
+		local newspeed = tonumber(message:match(' (%d*)'))
 		newUtcAdjust = -os.clock()*speed+utcAdjust+os.clock()*newspeed
 		speed = newspeed
-	else if message:match('.skip')
-	
+	elseif message:match('%.skip') then
+		local offset = tonumber(message:match(' (%d*)'))
+		--local cur = logs:seek('cur', offset)
+		for i = 1, offset do
+			m = cjson.decode(logs:read('*l'))
+		end
+	elseif message:lower():match('%.setutc') then
+		local utc = tonumber(message:match(' (.*)'))
+		utcAdjust=utc-os.clock()*speed
+		while utc>m[2] do
+			logs:seek('cur', 10000)
+			logs:read('*l')
+			m = cjson.decode(logs:read('*l'))
+		end
+		while utc < m[2] do
+			logs:seek('cur', -10000)
+			logs:read('*l')
+			m = cjson.decode(logs:read('*l'))
+		end
+		while utc > m[2] do
+			m = cjson.decode(logs:read('*l'))
+		end
+	elseif message:match('%.utc') then
+		s:sendChat(channel, tostring(os.clock()*speed+utcAdjust))
+	elseif message:match('%.help') then
+		s:sendChat(channel, 'Available Commands: ff, .skip, .setutc, .utc')
 	end
 end)
 
